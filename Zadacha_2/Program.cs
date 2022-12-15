ecbd2a142895657ab00c6aed487d907e8c5fbe8a
Danil <dan.loban@yandex.ru>
2022-12-15 19:13:47 +0300

    second

---
+++ Zadacha_2/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите число, ");
+int size = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [size];
+int sum = 0;
+for(int i = 0; i < size; i++)
+{
+    array[i] = new Random().Next(-100, 100);
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < size; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+for(int i = 0; i < size; i++)
+{
+    if(i % 2 != 0) sum = sum + array[i];
+}
+PrintArray(array);
+Console.Write(sum);
